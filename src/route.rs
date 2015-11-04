d3d1a8d8c0c644f0b4a23feaf24f39e9d6c9c1da
Jay Mundrawala <jdmundrawala@gmail.com>
2015-11-03 20:43:48 -0800

    Add test for parsing /proc/net/route

--- src/route.rs
+++ src/route.rs
@@ -9,6 +9,7 @@ use std::net::Ipv4Addr;
 use errors::RouteError;
 
 
+#[derive(PartialEq, Debug)]
 pub struct Route {
     pub iface: String,
     pub destination: std::net::IpAddr,
@@ -55,43 +56,76 @@ fn parse_ip(s: &str) -> Result<Ipv4Addr, RouteError> {
 }
 
 
-#[test]
-fn parse_ip_returns_error_for_bad_input_0_length() {
-    match parse_ip("000000") {
-        Err(RouteError::BadInput) => (),
-        otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
-    }
-}
+#[cfg(test)]
+mod test {
+    use std::io::BufReader;
+    use std::fs::File;
+    use std::net::IpAddr;
+    use std::net::Ipv4Addr;
 
-#[test]
-fn parse_ip_returns_error_for_bad_input_length_short() {
-    match parse_ip("000000") {
-        Err(RouteError::BadInput) => (),
-        otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
-    }
-}
+    use errors::RouteError;
 
+    #[test]
+    fn test_foo() {
+        let f = File::open("tests/fixtures/sample").unwrap();
+        let reader = BufReader::new(f);
 
-#[test]
-fn parse_ip_returns_error_for_bad_input_length_long() {
-    match parse_ip("0000000000") {
-        Err(RouteError::BadInput) => (),
-        otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
-    }
-}
-
-#[test]
-fn parse_ip_returns_parse_error_for_bad_input_length_bad_character() {
-    match parse_ip("000foo00") {
-        Err(RouteError::Parse(_)) => (),
-        otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
-    }
-}
-
-#[test]
-fn it_works() {
-    for x in load().unwrap() {
-        println!("{} {} {}\n", x.iface, x.destination, x.gateway);
+        let vec = super::parse_proc_net_route(reader).unwrap();
+        assert_eq!(vec[0], super::Route {
+            iface: "eno1".to_string(),
+            destination: IpAddr::V4(Ipv4Addr::new(0,0,0,0)),
+            gateway: IpAddr::V4(Ipv4Addr::new(192,168,1,1))
+        });
+        assert_eq!(vec[1], super::Route {
+            iface: "eno1".to_string(),
+            destination: IpAddr::V4(Ipv4Addr::new(192,168,1,0)),
+            gateway: IpAddr::V4(Ipv4Addr::new(0,0,0,0))
+        });
+        assert_eq!(vec[2], super::Route {
+            iface: "virbr0".to_string(),
+            destination: IpAddr::V4(Ipv4Addr::new(192,168,122,0)),
+            gateway: IpAddr::V4(Ipv4Addr::new(0,0,0,0))
+        });
     }
 
+    #[test]
+    fn parse_ip_returns_error_for_bad_input_0_length() {
+        match super::parse_ip("000000") {
+            Err(RouteError::BadInput) => (),
+            otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
+        }
+    }
+
+    #[test]
+    fn parse_ip_returns_error_for_bad_input_length_short() {
+        match super::parse_ip("000000") {
+            Err(RouteError::BadInput) => (),
+            otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
+        }
+    }
+
+
+    #[test]
+    fn parse_ip_returns_error_for_bad_input_length_long() {
+        match super::parse_ip("0000000000") {
+            Err(RouteError::BadInput) => (),
+            otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
+        }
+    }
+
+    #[test]
+    fn parse_ip_returns_parse_error_for_bad_input_length_bad_character() {
+        match super::parse_ip("000foo00") {
+            Err(RouteError::Parse(_)) => (),
+            otherwise => panic!("Expected RouteError::BadInput, got #{:?}", otherwise)
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        for x in super::load().unwrap() {
+            println!("{} {} {}\n", x.iface, x.destination, x.gateway);
+        }
+
+    }
 }
